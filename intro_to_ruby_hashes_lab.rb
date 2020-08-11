f84a0f0d5fcf63adaf37d172db4e4a91b3a7d278
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-11 18:25:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,13 +2,13 @@ def base_hash
   :railroads = {}
 end
 
-def monopoly_with_second_tier = {
+def monopoly_with_second_tier = 
   :railroads => {
     :pieces => 4, 
     :rent_in_dollars => {},
     :names => {},
   }
-}
+
 
 end
 
